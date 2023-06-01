b74b7fbfa590ddcc217f2c0cde544aa38613c71d
akashcs25 <akashshivaram25@gmail.com>
2023-06-01 22:29:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kushal(request):
+    return render(request,'kushal.html')
+def akash(request):
+    return render(request,'akash.html')
